86f842e788513b44c468b4e020805b4da20876e5
Tyler Yep <tyep@cs.stanford.edu>
2021-10-03 23:46:39 -0700

    add default encoding

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from flake8_future_annotations.checker import FutureAnnotationsChecker
 
 
 def run_validator_for_test_file(filename: str) -> list[tuple[int, int, str, type]]:
-    raw_content = Path(filename).read_text()
+    raw_content = Path(filename).read_text(encoding="utf-8")
     tree = ast.parse(raw_content)
 
     checker = FutureAnnotationsChecker(tree=tree, filename=filename)
