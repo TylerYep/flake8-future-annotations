67dfad832d6dc4d63d02c4fae837fe210f79bb67
Tyler Yep <tyler.yep@robinhood.com>
2022-10-02 11:54:21 -0700

    v1.0.0

--- flake8_future_annotations/checker.py
+++ flake8_future_annotations/checker.py
@@ -78,7 +78,7 @@ class FutureAnnotationsVisitor(ast.NodeVisitor):
 
 class FutureAnnotationsChecker:
     name = "flake8-future-annotations"
-    version = "0.0.5"
+    version = "1.0.0"
     force_future_annotations = False
 
     def __init__(self, tree: ast.Module, filename: str) -> None:
